0fcb1d2b9b6973d7b4cb61dd1765326650ba54be
nelcy <nelcy@DESKTOP-5U4JMOF>
2021-02-06 12:03:52 -0600

    VERSION 1.0

---
+++ src/mx/edu/cesba/main/HR_Main.java
@@ -0,0 +1,42 @@
+/*
+Alumna: Nelcy Aline Guerrero Rodriguez
+Matricula: 2022310
+ */
+package mx.edu.cesba.main;
+
+import javax.swing.JOptionPane;
+import mx.edu.cesba.model.Employee;
+
+/**
+ *
+ * @author nelcy
+ */
+public class HR_Main {
+
+    public static void main(String[] args) {
+        //Creacion de objetos:
+        Employee e1 = new Employee();//tipo de dato, identificador, asignador, valor del objeto (tipo de objeto) CONSTRUCTOR
+        Employee e2 = new Employee();
+        e1.id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID: "));//convertir de int a string
+        e1.firstName = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        e1.lastName = JOptionPane.showInputDialog("Ingrese su apellido: ");
+        e1.age = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad: "));//convertir de int a string
+        e1.email = JOptionPane.showInputDialog("Ingrese su correo: ");
+        e1.phoneNumber = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su numero telefonico: "));//convertir de int a string
+        e1.salary = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su salario: "));//convertir de int a string
+        e1.hireDate = JOptionPane.showInputDialog("Ingrese su fecha de contratacion: ");
+        
+        e2.id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su ID: "));//convertir de int a string
+        e2.firstName = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        e2.lastName = JOptionPane.showInputDialog("Ingrese su apellido: ");
+        e2.age = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad: "));//convertir de int a string
+        e2.email = JOptionPane.showInputDialog("Ingrese su correo: ");
+        e2.phoneNumber = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su numero telefonico: "));//convertir de int a string
+        e2.salary = Double.parseDouble(JOptionPane.showInputDialog("Ingrese su salario: "));//convertir de int a string
+        e2.hireDate = JOptionPane.showInputDialog("Ingrese su fecha de contratacion: ");
+        
+        e1.getDetails();
+        System.out.println("-----------------------------------------------------------------");
+        e2.getDetails();
+    }
+}
